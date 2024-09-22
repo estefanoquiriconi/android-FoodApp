565d1b0c9b07d658de1fcd6b90db9b6875170809
estefanoquiriconi <estefanoquiriconi@gmail.com>
2024-09-22 20:19:58 -0300

    binding, vista de recuperar contraseña OK

--- app/src/main/java/com/estefano/foodapp/RecoverPasswordActivity.java
+++ app/src/main/java/com/estefano/foodapp/RecoverPasswordActivity.java
@@ -1,6 +1,9 @@
 package com.estefano.foodapp;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -9,19 +12,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.estefano.foodapp.databinding.ActivityRecoverPasswordBinding;
+
 public class RecoverPasswordActivity extends AppCompatActivity {
+
+    ActivityRecoverPasswordBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_recover_password);
+        binding = ActivityRecoverPasswordBinding.inflate(getLayoutInflater());
+        binding.textFieldEmail.setEndIconVisible(false);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.editTextEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.textFieldEmail.setEndIconVisible(Patterns.EMAIL_ADDRESS.matcher(editable.toString()).matches());
+            }
+        });
     }
+
 
     public void goBack(View view){
         finish();
